318bf6c727e2c287b1b72250e320064592bceac3
Enlik Tjioe <enlik.stefanus@gmail.com>
2021-11-14 15:37:33 +0200

    setup - create account

--- week1_NFT_framework/hedera/hedera-go-examples/hedera_examples.go
+++ week1_NFT_framework/hedera/hedera-go-examples/hedera_examples.go
@@ -30,4 +30,47 @@ func main() {
 	//Print your testnet account ID and private key to the console to make sure there was no error
 	fmt.Printf("The account ID is = %v\n", myAccountId)
 	fmt.Printf("The private key is = %v\n", myPrivateKey)
+
+	//Create your testnet client
+	client := hedera.ClientForTestnet()
+	client.SetOperator(myAccountId, myPrivateKey)
+
+	//Generate new keys for the account you will create
+	newAccountPrivateKey, err := hedera.GeneratePrivateKey()
+	if err != nil {
+		panic(err)
+	}
+
+	newAccountPublicKey := newAccountPrivateKey.PublicKey()
+
+	//Create new account and assign the public key
+	newAccount, err := hedera.NewAccountCreateTransaction().
+		SetKey(newAccountPublicKey).
+		SetInitialBalance(hedera.HbarFrom(1000, hedera.HbarUnits.Tinybar)).
+		Execute(client)
+
+	//Request the receipt of the transaction
+	receipt, err := newAccount.GetReceipt(client)
+	if err != nil {
+		panic(err)
+	}
+
+	//Get the new account ID from the receipt
+	newAccountId := *receipt.AccountID
+
+	//Print the new account ID to the console
+	fmt.Printf("The new account ID is %v\n", newAccountId)
+
+	//Create the account balance query
+	query := hedera.NewAccountBalanceQuery().
+		SetAccountID(newAccountId)
+
+	//Sign with client operator private key and submit the query to a Hedera network
+	accountBalance, err := query.Execute(client)
+	if err != nil {
+		panic(err)
+	}
+
+	//Print the balance of tinybars
+	fmt.Println("The account balance for the new account is ", accountBalance.Hbars.AsTinybar())
 }
