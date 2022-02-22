047e6db5aea5bd87d0fd21014dd2d0855ad2178d
Enlik Tjioe <enlik.stefanus@gmail.com>
2022-02-23 00:08:14 +0200

    build images with hashlips art engine

--- RnD/my-generative-nfts/hashlips_art_engine/src/config.js
+++ RnD/my-generative-nfts/hashlips_art_engine/src/config.js
@@ -2,37 +2,64 @@ const basePath = process.cwd();
 const { MODE } = require(`${basePath}/constants/blend_mode.js`);
 const { NETWORK } = require(`${basePath}/constants/network.js`);
 
-const network = NETWORK.eth;
+// const network = NETWORK.eth;
+const network = NETWORK.sol;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
+// const namePrefix = "Your Collection";
+// const description = "Remember to replace this description";
+// const baseUri = "ipfs://NewUriToReplace";
+const namePrefix = "Triangle and Circle";
+const description = "Unique combinatons of a Circle and a Triangle with two backgrounds";
 const baseUri = "ipfs://NewUriToReplace";
 
+// const solanaMetadata = {
+//   symbol: "YC",
+//   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+//   external_url: "https://www.youtube.com/c/hashlipsnft",
+//   creators: [
+//     {
+//       address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+//       share: 100,
+//     },
+//   ],
+// };
+
 const solanaMetadata = {
-  symbol: "YC",
+  symbol: "TAC",
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  external_url: "",
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      address: "7CU5dmPRPBwjiQymQK7dW3rfsrcaNohSLbzxyTbtVape",
       share: 100,
     },
   ],
 };
 
 // If you have selected Solana then the collection starts from 0 automatically
+// const layerConfigurations = [
+//   {
+//     growEditionSizeTo: 5,
+//     layersOrder: [
+//       { name: "Background" },
+//       { name: "Eyeball" },
+//       { name: "Eye color" },
+//       { name: "Iris" },
+//       { name: "Shine" },
+//       { name: "Bottom lid" },
+//       { name: "Top lid" },
+//     ],
+//   },
+// ];
+
 const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Circle" },
+      { name: "Triangle" },
     ],
   },
 ];
@@ -78,7 +105,11 @@ const background = {
   default: "#000000",
 };
 
-const extraMetadata = {};
+// const extraMetadata = {};
+
+const extraMetadata = {
+	creator: "Enlik Tjioe"
+};
 
 const rarityDelimiter = "#";
 
